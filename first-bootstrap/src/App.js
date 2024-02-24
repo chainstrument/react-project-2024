2780fe6413d51838c42a9db438a5d4f980e3f8bf
Chahir <minitutoriel@gmail.com>
2024-02-24 08:47:10 +0100

    first

---
+++ first-bootstrap/src/App.js
@@ -0,0 +1,23 @@
+
+import React, { useState} from "React"
+
+function ExampleComponent ()  {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount( count + 1)}>Click me</button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+    bojour
+    </>
+  );
+}
+
+export default App;
