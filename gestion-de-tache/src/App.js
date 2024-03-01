9907b0c1d92f92e79aca6fc2f91543e8b9f85e15
Chahir <minitutoriel@gmail.com>
2024-03-01 11:52:48 +0100

    fin

--- gestion-de-tache/src/App.js
+++ gestion-de-tache/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Todo from './components/Todo';
+import TodoForm from './components/Form';
 import { useState } from 'react';
+
+import './App.css'
 
 function App() {
 
