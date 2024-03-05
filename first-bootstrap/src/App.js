9f24704d7a7385fdcdaf8ae46159960f3d4e4228
Chahir <minitutoriel@gmail.com>
2024-03-05 12:40:41 +0100

    'maj'

--- first-bootstrap/src/App.js
+++ first-bootstrap/src/App.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
- 
+
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-import ThemeSwitcher from './pages/ThemeSwitcher';
+import ThemeSwitcher from "./pages/ThemeSwitcher";
 
+import Survey from "./pages/Survey";
 
-import Survey from './pages/Survey';
-
-import ClientForm from './components/ClientForm'; 
-import FreelanceForm from './components/FreelanceForm';
-
+import ClientForm from "./components/ClientForm";
+import FreelanceForm from "./components/FreelanceForm";
 
 function App() {
   return (
@@ -22,15 +20,18 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="blogs" element={<Blogs />} / >
-              
+            <Route path="blogs" element={<Blogs />} />
+
             <Route path="themeswitcher" element={<ThemeSwitcher />} />
             <Route path="contact" element={<Contact />} />
-            <Route path="/survey" element={<Survey />} />
+            <Route path="/survey" element={<Survey />}>
+              <Route path="client" element={<ClientForm />} />
+              <Route path="freelance" element={<FreelanceForm />} />
+            </Route>
             <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
-      </BrowserRouter> 
+      </BrowserRouter>
     </>
   );
 }
